047a3ffd6e4c500a2e4d20482b1e6b34260e75e5
NazarenkoDS <nazarenko.danila@gmail.com>
2017-05-12 19:20:48 +0300

    task3008 в процессе

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3004/BinaryRepresentationTask.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task30.task3004;
+
+
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * Created by NazarenkoDS on 11.05.2017.
+ */
+
+
+
+
+
+public class BinaryRepresentationTask extends RecursiveTask<String> {
+    public BinaryRepresentationTask(int i) {
+    }
+
+    @Override
+    protected String compute() {
+        return null;
+    }
+}
